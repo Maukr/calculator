7d5f487ad003ee827ed880b56fc5aa7e42cde2a1
mauricio <maurifazzano@gmail.com>
2020-09-14 21:18:03 -0300

    Syntax error y boton positivo/negativo

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function operate(result, term, operator){
             return add(result, term);
         case '-':
             return subtract(result, term);
-        case 'X':
+        case 'x':
             return multiply(result, term);
         case '/':
             return divide(result, term);
@@ -32,6 +32,7 @@ let result = 0;
 
 const operation = document.querySelector('#operation');
 const resultHTML = document.querySelector('#result');
+resultHTML.innerHTML = 0;
 
 const number = Array.from(document.querySelectorAll('.number'));
 number.forEach((n) => {
@@ -52,6 +53,12 @@ operators.forEach((o) => {
     o.addEventListener('click', (e)=>{
         o.style.setProperty('background', '#ccc');
 
+        if(isNaN(Number(strnum))){
+            console.log(clearError(clearCalculator, 'Syntax Error'));
+            resultHTML.innerHTML = clearError(clearCalculator, 'Syntax Error');
+            return;
+        }   
+
         term = Number(strnum);
         if(!operator){
             result = term;
@@ -61,16 +68,59 @@ operators.forEach((o) => {
         operator = e.target.innerHTML;
         strnum = "";
         resultHTML.innerHTML = result;
-        console.log(result);
+        negative.style.setProperty('background', '#9ca1a4');
     });
 })
 
 const equals = Array.from(document.querySelectorAll('#eq'));
 equals.forEach((eq) => {
     eq.addEventListener('click', (e)=>{
-        //TODO
-        resultHTML.innerHTML = operate(result, Number(strnum), operator);
-        
+
+        if(isNaN(Number(strnum))){
+            console.log(clearError(clearCalculator, 'Syntax Error'));
+            resultHTML.innerHTML = clearError(clearCalculator, 'Syntax Error');
+            return;
+        }   
+
+        let res = operate(result, Number(strnum), operator)
+        resultHTML.innerHTML = res === 'Math ERROR' ? clearError(clearCalculator, 'Math ERROR') : res;
+        operation.innerHTML = '';
+        strnum = '';
+        negative.style.setProperty('background', '#9ca1a4');
     });
 })
 
+const ac = document.querySelector('#ac').addEventListener('click', e => {
+    operators.forEach(o => {
+        o.style.setProperty('background', '#9ca1a4');
+    })
+    clearCalculator();
+});
+
+const negative = document.querySelector('#negative');
+
+negative.addEventListener('click', e => {
+    if(strnum.includes('-')){
+        strnum = strnum.substring(1);
+        negative.style.setProperty('background', '#9ca1a4');
+    }else{
+        strnum = '-' + strnum;
+        negative.style.setProperty('background', '#ccc');
+    }
+});
+
+function clearCalculator(){
+    strnum = "";
+    term;
+    operator;
+    result = 0;
+    resultHTML.innerHTML = 0;
+    operation.innerHTML = '';
+}
+
+function clearError(errorFunc, res){
+    setTimeout(function(){ 
+        errorFunc();
+    }, 1500);
+    return res;
+}
